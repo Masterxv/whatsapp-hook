ff2906de1113572e5dca5c9df10ee76732dda0a4
Mustafa Hanif <mustafa.hanif@seera.sa>
2024-03-06 19:21:06 +0400

    mm

--- pb_hooks/main.pb.js
+++ pb_hooks/main.pb.js
@@ -3,7 +3,3 @@ routerAdd("GET", "/hello/:name", (c) => {
 
   return c.json(200, { "message": "Hello hi" + name })
 })
-
-routerAdd("GET", "/", (c) => {
-  return c.json(200, { "message": "ok" })
-})
