4a2806e694aabfe7e67a4a1c8f2062b6c6d75a25
Mustafa Hanif <mustafa.hanif@seera.sa>
2024-03-09 02:11:24 +0400

    mm

--- nextjs-docker/pages/api/hello.ts
+++ nextjs-docker/pages/api/hello.ts
@@ -1,8 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import PocketBase from 'pocketbase'
 import { inngest } from "./inngest";
 
 // Opt out of caching; every request should send a new event
 export const dynamic = "force-dynamic";
+
+const pb = new PocketBase('https://node.taskmate.ae/');
 
 // Create a simple async Next.js API route handler
 export default async function handler(
@@ -10,12 +13,19 @@ export default async function handler(
   res: NextApiResponse,
 ) {
   // Send your event payload to Inngest
-  await inngest.send({
-    name: "test/hello.world",
-    data: {
-      email: "testFromNext@example.com",
-    },
-  });
-
-  res.status(200).json({ name: "Hello Inngest from Next!" });
+  try {
+    const authData = await pb.admins.authWithPassword('icemelt7@gmail.com', 'Jojo.33443344');
+    console.log(pb.authStore.isValid);
+    console.log(pb.authStore.token);
+    console.log(pb.authStore.model.id);
+    const resultList = await pb.collection('leads').getList(1, 50, {
+      filter: 'phone_number = "971555555555"',
+    });
+  
+    return res.status(200).json(resultList);
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json(e);
+  }
+  
 }
--- nextjs-docker/pages/api/whatsapp.ts
+++ nextjs-docker/pages/api/whatsapp.ts
@@ -1,10 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { inngest } from "./inngest";
+import PocketBase from 'pocketbase'
+
 import axios from "axios";
 
 export const dynamic = 'force-dynamic'
 
 const GRAPH_API_TOKEN = 'EAAI5Hdm2fCsBOwo1gwKgrAUmGidJvJXOSp12ZCNLOAz9ZBtBed8ECiZB0Gz2JtFZBt7rYVdCyIirjS3FnVouiJZBH72ofGrrpZCIgRZBRR7hphAOZBJ8B7bJDv0zpVWjAi7XIoCI3KlJg8Wd55q6neOk88ZAM9gb1ZBQXh84SZBweoRmba9vymBNGlOLNhXINjeCBRxZCpfD8gpUnwq63iGFUiXc4ZCFqM8BnqQeZA';
+
+const pb = new PocketBase('https://node.taskmate.ae/');
 
 // Create a simple async Next.js API route handler
 export default async function handler(
@@ -23,6 +27,25 @@ export default async function handler(
     // extract the business number to send the reply from it
     const business_phone_number_id =
       req.body.entry?.[0].changes?.[0].value?.metadata?.phone_number_id;
+
+    try {
+      const authData = await pb.admins.authWithPassword('icemelt7@gmail.com', 'Jojo.33443344');
+      const resultList = await pb.collection('leads').getList(1, 50, {
+        filter: `phone_number = "${business_phone_number_id}"`,
+      });
+      if (resultList.items.length === 0) {
+        const data = {
+          "phone_number": `${business_phone_number_id}`,
+          "name": `${business_phone_number_id}`
+        };
+        
+        const record = await pb.collection('leads').create(data);
+      }
+      // return res.status(200).json(resultList);
+    } catch (e) {
+      console.log(e);
+      return res.status(400).json(e);
+    }
 
     // send a reply message as per the docs here https://developers.facebook.com/docs/whatsapp/cloud-api/reference/messages
     await axios({
