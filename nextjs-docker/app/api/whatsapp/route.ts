d4d09699a757bc23c0ba4aff5cff5d41c3dcdbbd
Mustafa Hanif <mustafa.hanif@seera.sa>
2024-03-28 15:11:24 +0400

    fix subscribe

--- nextjs-docker/app/api/whatsapp/route.ts
+++ nextjs-docker/app/api/whatsapp/route.ts
@@ -8,11 +8,10 @@ export async function GET(req: NextRequest) {
   const mode = req.nextUrl.searchParams.get("hub.mode");
   const token = req.nextUrl.searchParams.get("hub.verify_token");
   const challenge = req.nextUrl.searchParams.get("hub.challenge");
-
   // check the mode and token sent are correct
   if (mode === "subscribe" && token === 'HAPPY') {
     // respond with 200 OK and challenge token from the request
-    return NextResponse.json({ challenge });
+    return Response.json(Number(challenge), { status: 200 });
   } else {
     // respond with '403 Forbidden' if verify tokens do not match
     return NextResponse.json({ error: 'Forbidden' }, { status: 403 });
