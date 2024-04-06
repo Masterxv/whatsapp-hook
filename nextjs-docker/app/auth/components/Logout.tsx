ba851f23fef682bf03a177cbe499e80955ba70a7
Mustafa Hanif <mustafa.hanif@seera.sa>
2024-04-06 16:56:25 +0400

    fb logout

--- nextjs-docker/app/auth/components/Logout.tsx
+++ nextjs-docker/app/auth/components/Logout.tsx
@@ -2,6 +2,7 @@
 
 import { logoutUser } from "@/state/user/user";
 import { useRouter } from "next/navigation";
+import {useEffect} from "react";
 
 interface LogoutProps {
 
@@ -9,10 +10,38 @@ interface LogoutProps {
 
 export function Logout({}:LogoutProps){
     const router = useRouter();
+    useEffect(() => {
+        window.fbAsyncInit = function () {
+            window.FB.init({
+                appId: '625750322871339',
+                cookie: true,
+                xfbml: true,
+                version: 'v19.0'
+            });
+
+            window.FB.AppEvents.logPageView();
+
+            window.FB.getLoginStatus(function (response: any) {
+                console.log(response);
+            });
+        };
+
+        (function (d, s, id) {
+            var js: HTMLScriptElement, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) { return; }
+            js = d.createElement(s) as HTMLScriptElement; js.id = id;
+            js.src = "https://connect.facebook.net/en_US/sdk.js";
+            if (fjs && fjs.parentNode) {
+                fjs.parentNode.insertBefore(js, fjs);
+            }
+        }(document, 'script', 'facebook-jssdk'));
+
+    }, [])
 return (
 <button 
 className=" hover:bg-slate-700 "
-onClick={() => { 
+onClick={() => {
+
     logoutUser()
     router.refresh()
      
